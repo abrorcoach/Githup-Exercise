70b3e84b85eee00062e2d1fc15410f163d3c0496
Abror <abrorhalimov660@gmail.com>
2025-02-24 17:36:38 +0500

    Add project files.

---
+++ Githup-Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Githup_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
